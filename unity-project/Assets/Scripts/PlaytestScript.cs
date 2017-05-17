f187245fc5c126db7829a954888f7080bbeefaba
Taldops <2dobert@informatik.uni-hamburg.de>
2017-05-17 12:34:49 +1000

    Added Playtesting level and functionality

---
+++ unity-project/Assets/Scripts/PlaytestScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaytestScript : MonoBehaviour {
+
+	public float time;		//Playing time displayed in the upper right corner
+	public bool completed = false;	//Has the player reached the goal yet?
+
+	private Vector2 startingPos;
+
+	// Use this for initialization
+	void Start () {
+		startingPos = (Vector2) transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!completed)
+		{
+			time += Time.deltaTime;
+		}
+
+		//Reset Button
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			transform.position = startingPos;
+			time = 0;
+			completed = false;
+		}
+	}
+
+	void OnGUI()
+	{
+		//int w = Mathf.RoundToInt(Screen.width * size), h = Mathf.RoundToInt(Screen.height * size);
+		int w = Screen.width, h = Screen.height;
+
+		GUIStyle style = new GUIStyle();
+
+		Rect rect = new Rect(0, 0, w, h * 2 / 100);
+		style.alignment = TextAnchor.UpperRight;
+		style.fontSize = Mathf.RoundToInt(2 * h * 0.02f);
+		style.normal.textColor = completed ? Color.green : Color.black;
+		string text = string.Format("{0:0.0} s", time);
+		GUI.Label(rect, text, style);
+	}
+}
