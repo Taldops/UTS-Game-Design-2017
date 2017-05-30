8a3c0e666942cfbf0f2e66793b317c86097e18b5
Otakuan <huw.krix@gmail.com>
2017-05-30 10:14:53 +1000

    Changed the getHit to affect player whenever taking damage
    Please test

--- unity-project/Assets/Scripts/PlayerCharacter.cs
+++ unity-project/Assets/Scripts/PlayerCharacter.cs
@@ -24,6 +24,7 @@ public class PlayerCharacter : MonoBehaviour {
 	public void Hurt(int damage) {
 		if(timeTillHurt <= 0){
 		_health -= damage;
+		this.gameObject.GetComponent<PlayerControl>().GetHit();
 		 timeTillHurt = invincibleTime;
 		}
 		//Debug.Log("Health: " + _health);
