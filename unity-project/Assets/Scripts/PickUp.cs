075fbddfdc300d9a56568ed03f927938e2a6202d
Yongee55 <yongee@192-168-1-4.tpgi.com.au>
2017-05-30 17:49:27 +1000

    Update lv3

--- unity-project/Assets/Scripts/PickUp.cs
+++ unity-project/Assets/Scripts/PickUp.cs
@@ -6,27 +6,37 @@ public class PickUp : MonoBehaviour {
 	private int value = 100;
     public float secondsToAdd = 5;
 	private bool collected = false;
-		
-		
+
+	public AudioClip collectedSound;
+	public AudioSource source;
+
 	void Update() {
-		
 		
 	}
 
     void OnTriggerEnter2D(Collider2D other)
     {
+		//???
+//		source.GetComponent<AudioSource> ();
         if (other.transform.parent != null)
         {
             PlayerCharacter player = other.transform.parent.GetComponent<PlayerCharacter>();
+
             if (player != null)
             {
-                if (!collected) { 
+                if (!collected) {
+					
+					//Try adding sound
+					//source.PlayOneShot(collectedSound);
+
                     player.getPoints(value);
                     GameObject.Find("Goal Zone").transform.GetComponent<PlaytestScript>().LevelTime += secondsToAdd;
                     collected = true;
                 }
                 Debug.Log("PlayerPickup");
-                Destroy(this.gameObject);
+                Destroy(this.gameObject); 
+
+
             }
         }
     }
