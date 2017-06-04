41270c94766376b9b093a7bf15b48d79d933e1f0
Taldops <2dobert@informatik.uni-hamburg.de>
2017-06-04 16:04:03 +1000

    Improved enemy shooting behavior

--- unity-project/Assets/Scripts/ShootingEnemy.cs
+++ unity-project/Assets/Scripts/ShootingEnemy.cs
@@ -4,7 +4,10 @@ using System.Collections;
 public class ShootingEnemy : MonoBehaviour {
 	//public float speed = 3.0f;
 	//public float obstacleRange = 5.0f;
-	
+
+	public float shootRadiusX = 35;	//How close the player needs to be for the enemy to shoot at them
+	public float shootRadiusY = 20;	//How close the player needs to be for the enemy to shoot at them
+
 	[SerializeField] private GameObject shurikenPrefab;
 	private GameObject _shuriken;
 	Vector3 offset = new Vector3(0, 30, 0);
@@ -12,17 +15,20 @@ public class ShootingEnemy : MonoBehaviour {
 	float tillFire = 0;
 	
 	//private bool _alive;
+	private GameObject player; 
 	
 	void Start() {
 		//_alive = true;
+		player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	void Update() {
 		tillFire -= Time.deltaTime;
 		if(tillFire <= 0){
-			if (_shuriken == null) {
+			if (_shuriken == null && inRange()) {
 				SoundManager.instance.PlaySingle(SoundManager.instance.enemyShoot);
 				_shuriken = Instantiate(shurikenPrefab) as GameObject;
+				_shuriken.GetComponent<SpriteRenderer>().color = Color.red;
 				//GetComponent<Rigidbody>();
 				_shuriken.GetComponent<Shuriken>().normalizedDirection = Vector3.Normalize(GameObject.Find("Hero").transform.position - this.transform.position);
 				_shuriken.GetComponent<Shuriken>().isEnemy = true;					//Vector3.Normalize(target.Position - character.Position)
@@ -32,6 +38,11 @@ public class ShootingEnemy : MonoBehaviour {
 				tillFire = fireRate;
 			}
 		}
+	}
+
+	private bool inRange()
+	{
+		return (Mathf.Abs(player.transform.position.x - transform.position.x) < shootRadiusX && Mathf.Abs(player.transform.position.y - transform.position.y) < shootRadiusY);
 	}
 /*
 	public void SetAlive(bool alive) {
